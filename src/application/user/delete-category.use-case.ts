ef19d5716348bfc00f7c88c0e921ea0cf3bb7fe2
Sergio Soares <sergio.veiga.soares@gmail.com>
2025-07-08 06:05:48 -0300

    feat: implement DeleteUserUseCase for user deletion logic

---
+++ src/application/user/delete-category.use-case.ts
@@ -0,0 +1,20 @@
+import { Uuid } from "../../domain/shared/value-objects/uuid.vo";
+import type { IUserRepository } from "../../domain/user/user.repository";
+import type { IUseCase } from "../shared/use-case.interface";
+
+export class DeleteUserUseCase
+  implements IUseCase<DeleteUserInput, DeleteUserOutput>
+{
+  constructor(private readonly userRepository: IUserRepository) {}
+
+  async execute(input: DeleteUserInput): Promise<void> {
+    const uuid = new Uuid(input.id);
+    await this.userRepository.findById(uuid);
+  }
+}
+
+export type DeleteUserInput = {
+  id: string;
+};
+
+export type DeleteUserOutput = void;
